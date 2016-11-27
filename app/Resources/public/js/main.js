12929cafecdd7f044e38671f66d17314d29cd86f
resurtm <resurtm@gmail.com>
2016-11-27 13:59:18 +0600

    webpack initial settings

---
+++ app/Resources/public/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var jQuery = require('jquery');
+
+jQuery(function ($) {
+    console.log(1);
+    console.log(123123);
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './app/Resources/public/css/main.less',
+        './app/Resources/public/js/main.js',
+        'webpack-dev-server/client?http://localhost:8090'
+    ],
+    output: {
+        publicPath: '/',
+        filename: 'main.js'
+    },
+    debug: true,
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, './app/Resources/public/js/'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!autoprefixer!less"
+            },
+        ]
+    },
+    devServer: {
+        contentBase: './app/Resources/public/'
+    }
+};
